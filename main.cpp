e731594d6fdfadf41a659cf31266d5446e8b29d3
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-08-22 18:59:28 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    int lines;
+    cin>>lines;
+    map<string, int> girls;
+    string pais;
+    string name;
+
+    for (int i=0; i<lines; i++){
+        cin>>pais;
+        getline(cin, name);
+        girls[pais]++;
+    }
+
+    for (map<string, int>::const_iterator it = girls.begin(); it != girls.end(); ++it){
+        cout<<it->first<<" "<< it->second<<"\n";
+    }
+
     return 0;
 }
